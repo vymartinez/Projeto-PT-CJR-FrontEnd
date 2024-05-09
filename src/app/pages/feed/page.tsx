80fe641d07dbc2d920c6140e4e41cc9fb92437cd
Victor Martinez <victoryanmartinez@gmail.com>
2024-05-09 15:13:47 -0300

    adding pages file

---
+++ src/app/pages/feed/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Feed = () => {
+  return (
+    <>
+        <h1>Feed</h1>
+    </>
+  )
+}
+
+export default Feed;
---
+++ src/app/pages/user-profile/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const userProfile = () => {
+  return (
+    <>
+        <h1>User Profile</h1>
+    </>
+  )
+}
+
+export default userProfile
