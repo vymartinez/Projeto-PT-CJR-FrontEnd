eeb107482da8247cafa0f683350945b12532308a
sarahnovaiss <sarahnm.novais@gmail.com>
2024-05-26 00:17:56 -0300

    tela de login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,74 @@
-import React from 'react'
-import {HeaderLogged, HeaderUnlogged} from './components/Headers'
+import React, { createContext } from "react";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
 
 
-const Home = () => {
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+const validationSchema = Yup.object({
+    email: Yup.string().email("Endereço de email inválido").required("Required"),
+    password: Yup.string().required("Required"),
+  });
+  
+  const initialValues = { email: "", password: ""};
+
+  const handleSubmit = (values: any) => {
+    console.log(values);
+  }
 
   return (
-    <>
-      <div>Login</div>
-    </>
-  )
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="w-full bg-white shadow-md rounded-lg overflow-hidden">
+        <div className="flex h-full">
+          <div className="w-1/2 bg-gray-200">
+          <img className="w-full h-full" src="/images/university.png" />
+         </div>
+         <div className="w-1/2 flex">
+          <div className="m-auto">
+            <h2 className="w-96 h-16 text-center text-black text-5x1 font-normal ">Avaliação de Professores</h2>
+            <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={handleSubmit}>
+              <Form className="flex flex-col">
+                <div className="mb-4">
+                  <Field
+                    name="email"
+                    type="email"
+                    id= "email"
+                    placeholder ="Email"
+                    className= "mt-1 block w-full px-3 py-2 border border-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-550 sm:text-sm rounded-md"
+                  />
+                </div>
+                <div className="mb-4">
+                  <Field
+                    name="password" 
+                    type="password"
+                    id= "password"
+                    placeholder="Senha"
+                    className= "mt-1 block w-full px-3 py-2 border border-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-550 sm:text-sm rounded-md"
+                  />
+                  <div className="inline-flez space-x-4">
+                    <button
+                    type= "submit"
+                    className= "w-full bg-emerald-200 text-blue-950 py-2 px-4 rounded-md hover:bg-emerald-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50"
+                    >
+                    Entrar
+                    </button>
+                    <button
+                     type= "button"
+                     className="w-full bg-emerald-200 text-blue-950 py-2 px-4 rounded-md hover:bg-emerald-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50"
+                    >
+                     Criar Conta
+                    </button>
+                  </div>
+                </div>
+              </Form>
+            </Formik>
+          </div>
+         </div>
+       </div>
+      </div>
+    </div>
+  );
 }
-
-export default Home;
