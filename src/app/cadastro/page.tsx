a5cc17871932c194b37e0e0e70a9ed9fd05ca5aa
aadriellyy <limaadrielly214@gmail.com>
2024-05-30 20:49:12 -0300

    configurando formik e yup

---
+++ src/app/cadastro/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+import React from 'react';
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import unb from "@/../public/images/unb.avif"
+import logo from "@/../public/images/unb-logo.png"
+
